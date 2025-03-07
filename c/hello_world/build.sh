d87c10d1c2f00a9f9ddd4162c2c280b0b983f156
Stephen Willcock <stephen.willcock@gmail.com>
2025-03-07 09:29:01 +0000

    hello_world C

---
+++ c/hello_world/build.sh
@@ -0,0 +1,8 @@
+set -x #echo on
+
+f="hello_world"
+marvin="../lib/marvin.c"
+org=0x8000
+reset=0x0000
+
+zcc +z80 -clib=classic $f.c $marvin -pragma-define:CRT_ORG_CODE=$org -pragma-define:CRT_ON_EXIT=$reset -create-app -m -Cz--ihex -o=$f.o
---
+++ c/hello_world/hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nHello world!\n(hit any key)\n\n");
+    int c = fgetc_cons(); // wait for a key before ending
+}
