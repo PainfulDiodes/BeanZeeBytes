f9091f7ad45e9752b4b0492ac6a7f01acc0b2bad
Stephen Willcock <stephen.willcock@gmail.com>
2025-03-09 08:36:56 +0000

    Update marvin.c
    comments

--- c_examples/lib/marvin.c
+++ c_examples/lib/marvin.c
@@ -2,11 +2,15 @@
 
 #include <stdio.h>
 
+// override low-level console functions as per:
+// https://github.com/z88dk/z88dk/wiki/Classic--Homebrew
+// these bubble up to higher level functions: putchar(), printf() etc.
+
 int fputc_cons_native(char c) __naked
 {
 __asm
-    pop     bc  ;return address
-    pop     hl  ;character to print in l
+    pop     bc    ;return address
+    pop     hl    ;character to print in l
     push    hl
     push    bc
     ld      a,l
@@ -18,7 +22,7 @@ __endasm;
 int fgetc_cons() __naked
 {
 __asm
-    call    $0010 ; marvin getchar
+    call    $0010   ; marvin getchar
     ld      l,a     ;Return the result in hl
     ld      h,0
     ret
