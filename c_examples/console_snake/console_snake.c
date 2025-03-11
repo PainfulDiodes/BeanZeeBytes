54abbcbbafd2ddc50668d9097298ecc697bceba5
Stephen Willcock <stephen.willcock@gmail.com>
2025-03-11 07:51:00 +0000

    welcome screen

--- c_examples/console_snake/console_snake.c
+++ c_examples/console_snake/console_snake.c
@@ -9,9 +9,15 @@ void cursorMove(char, int);
 void openingScreen(void);
 void plot(int,int);
 void plotBox(int,int,int,int);
+void delay(int);
+void hideCursor(void);
+void showCursor(void);
+
 
 const int SCREEN_WIDTH = 80;
 const int SCREEN_HEIGHT = 24;
+
+int read_char = 0;
 
 int main()
 {
@@ -20,31 +26,60 @@ int main()
 
 void openingScreen() {
     clearScreen();
-    setCursor(35,12);
-    printf("Snake!!");
+    hideCursor();
+    setCursor(35,13);
+    printf("Snake!");
     setCursorHome();
     
     for(int r=0; r<12; r++) {
         plotBox(1+r,SCREEN_WIDTH-r,1+r,SCREEN_HEIGHT-r);
+        if(read_char!=0) break;
     }
-        
-    int i = getchar();
-    
+
+    while(read_char==0) read_char = readchar();
+
     clearScreen();
+    showCursor();
     setCursorHome();
 }
 
 void plotBox(int x1, int x2, int y1, int y2) {
-    for(int x=x1; x<=x2; x++) plot(x,y1);
-    for(int y=y1; y<=y2; y++) plot(x2,y);
-    for(int x=x2; x>=x1; x--) plot(x,y2);
-    for(int y=y2; y>=y1; y--) plot(x1,y);
+    for(int x=x1; x<=x2; x++) {
+        plot(x,y1);
+        if(read_char!=0) return;
+    }
+
+    for(int y=y1; y<=y2; y++) {
+        plot(x2,y);
+        if(read_char!=0) return;
+    }
+
+    for(int x=x2; x>=x1; x--) {
+        plot(x,y2);
+        if(read_char!=0) return;
+    }
+
+    for(int y=y2; y>=y1; y--) {
+        plot(x1,y);
+        if(read_char!=0) return;
+    }
 }
 
 void plot(int x, int y) {
     setCursor(x,y);
     putchar('*');
+    delay(32);
+    read_char = readchar();
 }
+
+void delay(int d) {
+    for(int i=0; i<d; i++) {
+        for(int j=0; j<100; j++) {
+            //
+        }
+    }
+}
+
 
 void clearScreen() {
     printf("\e[2J");
@@ -61,3 +96,12 @@ void setCursorHome() {
 void cursorMove(char dir, int dist) {
     printf("\e[%d%c",dist,dir);
 }
+
+void hideCursor() {
+    printf("\e[?25l");
+}
+
+void showCursor() {
+    printf("\e[?25h");
+}
+
