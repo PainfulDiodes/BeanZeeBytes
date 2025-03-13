70c055af2c00bd07db27518be4b9d799c7394e1c
Stephen Willcock <stephen.willcock@gmail.com>
2025-03-13 08:14:39 +0000

    Update console_snake.c
    interim

--- c_examples/console_snake/console_snake.c
+++ c_examples/console_snake/console_snake.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "readchar.h"
 #include "terminal.h"
 
@@ -7,21 +8,48 @@ void splash(void);
 void splashDot(int,int);
 void splashBox(int,int,int,int);
 void delay(unsigned long);
+void setCell(int,int,char);
+void drawSnake(void);
+void setupGame(void);
+    
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 24
+#define X_MAX_CELL 80
+#define Y_MAX_CELL 24
+#define X_START_CELL 40
+#define Y_START_CELL 12
 
+// snake is held in 2 arrays for coordinates. First element is the head of the snake. 
+#define MAX_LENGTH 100
+int x_cell_pos[MAX_LENGTH+1];
+int y_cell_pos[MAX_LENGTH+1];
 
-const int SCREEN_WIDTH = 80;
-const int SCREEN_HEIGHT = 24;
+int length = 5;
+bool alive;
+int score,motion; //fruit_x, fruit_y, 
+
+#define MOTION_NONE  0
+#define MOTION_UP    1
+#define MOTION_DOWN  2
+#define MOTION_LEFT  3
+#define MOTION_RIGHT 4
 
 int main()
 {
     clearScreen();
     hideCursor();
     splash();
-    while(readchar()!=0); // wait for keypress
+    while(readchar()==0); // wait for keypress
+
+    setupGame();
+
+    while(readchar()==0); // wait for keypress
+
     clearScreen();
     showCursor();
     setCursorHome();
 }
+
 void splash() {
     setCursor(35,13);
     printf("Snake!");
@@ -42,10 +70,44 @@ void splashBox(int x1, int x2, int y1, int y2) {
 }
 
 void splashDot(int x, int y) {
-    setCursor(x,y);
-    putchar('*');
+    setCell(x,y,'*');
     delay(3);
 }
+
+void setupGame() {
+    // lastMoveMillis = millis();
+    score = 0;
+    motion = MOTION_NONE;
+    alive = true;
+  
+    for(int i=0; i<=length; i++) // use <= as the position after the length is used to clean up the prior position of the snake
+    {
+      x_cell_pos[i] = X_START_CELL-i;
+      y_cell_pos[i] = Y_START_CELL;
+    }
+  
+    clearScreen();
+    drawSnake();
+    // dropFruit();
+    // printScore(ILI9341_WHITE);
+  }
+  
+void setCell(int x, int y, char c) {
+    setCursor(x,y);
+    putchar(c);
+}
+
+void drawSnake() {
+    int c = 1;
+    for(int i=0; i<length; i++) {
+      if(i==0) setCell(x_cell_pos[i],y_cell_pos[i], alive ? '#' : '.');
+      else if(c==1) setCell(x_cell_pos[i],y_cell_pos[i], alive ? '*' : '.');
+      else setCell(x_cell_pos[i],y_cell_pos[i],alive ? 'o' : '.');
+      c=-c;
+    }
+    setCell(x_cell_pos[length],y_cell_pos[length],' ');
+  }
+  
 
 void delay(unsigned long d) {
     for(unsigned long l=0; l<d; l++) {
