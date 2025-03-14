057f20ace6679616623a4e7c7c5aaaf482380224
Stephen Willcock <stephen.willcock@gmail.com>
2025-03-14 06:43:44 +0000

    improve splash key wait

--- c_examples/console_snake/console_snake.c
+++ c_examples/console_snake/console_snake.c
@@ -7,7 +7,7 @@
 // function prototypes
 void splash(void);
 void splashDot(int,int);
-void splashBox(int,int,int,int);
+bool splashBox(int,int,int,int);
 void delay(unsigned long);
 void setCell(int,int,char);
 void drawSnake(void);
@@ -57,18 +57,30 @@ void splash() {
     printf("Snake!");
     setCursorHome();
     for(int r=0; r<12; r++) {
-        splashBox(1+r,SCREEN_WIDTH-r,1+r,SCREEN_HEIGHT-r);
-        if(readchar()!=0) break;
+        if(splashBox(1+r,SCREEN_WIDTH-r,1+r,SCREEN_HEIGHT-r)) break;
     }
     setCursor(35,13);
     printf("Snake!");
 }
 
-void splashBox(int x1, int x2, int y1, int y2) {
-    for(int x=x1; x<=x2; x++) splashDot(x,y1);
-    for(int y=y1; y<=y2; y++) splashDot(x2,y);
-    for(int x=x2; x>=x1; x--) splashDot(x,y2);
-    for(int y=y2; y>=y1; y--) splashDot(x1,y);
+bool splashBox(int x1, int x2, int y1, int y2) {
+    for(int x=x1; x<=x2; x++) {
+        splashDot(x,y1); 
+        if(readchar()!=0) return true;
+    }
+    for(int y=y1; y<=y2; y++) {
+        splashDot(x2,y);
+        if(readchar()!=0) return true;
+    }
+    for(int x=x2; x>=x1; x--) {
+        splashDot(x,y2);
+        if(readchar()!=0) return true;
+    }
+    for(int y=y2; y>=y1; y--) {
+        splashDot(x1,y);
+        if(readchar()!=0) return true;
+    }
+    return false;
 }
 
 void splashDot(int x, int y) {
