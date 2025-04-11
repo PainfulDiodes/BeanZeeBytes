0de124ac10c6331dc37eccb688855dc74e8cf03c
Stephen Willcock <stephen.willcock@gmail.com>
2025-04-11 08:31:18 +0100

    Update keyscan.asm
    tidy comments

--- asm_examples/beanboard_keyscan/keyscan.asm
+++ asm_examples/beanboard_keyscan/keyscan.asm
@@ -12,24 +12,24 @@ start:
     call puts 
 
 keyscanstart:
-    ld b,0x01               ; current column bit
-    ld hl,key_buffer        ; location of previous value
+    ld b,0x01                   ; current column bit
+    ld hl,key_buffer            ; location of previous value
 keyscanloop:
     ld a,b
     call keyscan
     cp 0
-    jr z,keyscannext           ; yes - skip print
+    jr z,keyscannext            ; yes - skip print
     add 'a'-1
     call putchar
 keyscannext:
     inc hl
-    or a                    ; clear carry
-    rl b                    ; shift column bit left - will move to carry flag after bit 7
-    jr nc,keyscanloop       ; loop if not done all bits
+    or a                        ; clear carry
+    rl b                        ; shift column bit left - will move to carry flag after bit 7
+    jr nc,keyscanloop           ; loop if not done all bits
 
 delay:
     ld hl,0x0a00
-delayloop:               ; key debounce
+delayloop:                      ; key debounce
     dec hl
     nop
     ld a,h
@@ -39,7 +39,7 @@ delayloop:               ; key debounce
     cp a
     jr nz,delayloop
 
-    call readchar           ; repeat until receive input from USB
+    call readchar               ; repeat until receive input from USB
     cp 0
     jr z,keyscanstart
 
@@ -52,14 +52,14 @@ console_message_2:
     db "Hit any key to stop BeanBoard keyscan\n",0
 
 
-keyscan:                    ; A contains column bit, HL contains a pointer to the old value, return value in A
+keyscan:                        ; A contains column bit, HL contains a pointer to the old value, return value in A
     push bc
-    out (KBD_PORT),a        ; output column strobe
-    in a,(KBD_PORT)         ; get row values
-    ld b,(hl)               ; fetch previous value
-    cp b                    ; current value same as previous?
-    jr z,keyscansame        ; yes - skip
-    ld (hl),a               ; store new value
+    out (KBD_PORT),a            ; output column strobe
+    in a,(KBD_PORT)             ; get row values
+    ld b,(hl)                   ; fetch previous value
+    cp b                        ; current value same as previous?
+    jr z,keyscansame            ; yes - skip
+    ld (hl),a                   ; store new value
     pop bc
     ret
 keyscansame:
@@ -73,14 +73,14 @@ keyscansame:
 ; if there is no data, return 0
 ; NOTE this needs to be moved into MARVIN
 
-UM245R_CTRL equ 0          ; serial control port
-UM245R_DATA equ 1          ; serial data port
+UM245R_CTRL equ 0               ; serial control port
+UM245R_DATA equ 1               ; serial data port
 
 readchar:
-    in a,(UM245R_CTRL)      ; get the USB status
-    bit 1,a                 ; data to read? (active low)
-    jr nz,readcharnodata    ; no, the buffer is empty
-    in a,(UM245R_DATA)      ; yes, read the received char
+    in a,(UM245R_CTRL)          ; get the USB status
+    bit 1,a                     ; data to read? (active low)
+    jr nz,readcharnodata        ; no, the buffer is empty
+    in a,(UM245R_DATA)          ; yes, read the received char
     ret 
 readcharnodata:
     ld a,0
