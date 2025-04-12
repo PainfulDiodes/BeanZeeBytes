1141ae9446a274b2d98d83dbacbecb9015f9cacd
Stephen Willcock <stephen.willcock@gmail.com>
2025-04-12 09:45:25 +0100

    Update echo.asm

--- asm_examples/beanboard_echo/echo.asm
+++ asm_examples/beanboard_echo/echo.asm
@@ -3,7 +3,6 @@ include "../lib/marvin.asm"     ; load all the definitions needed to interact wi
 LCD_CTRL equ 4                  ; LCD control port
 LCD_DATA equ 5                  ; LCD data port
 
-
 ; LCD commands
 LCD_CLEAR_DISPLAY equ 0x01
 LCD_RETURN_HOME equ 0x02
@@ -46,20 +45,29 @@ LCD_FONT_8 equ 0x00
 org RAMSTART                    ; this is the default location for a BeanZee standalone assembly program 
 
 start:
-	ld a,LCD_FUNCTION_SET+LCD_DATA_LEN_8+LCD_DISP_LINES_2+LCD_FONT_10
+	ld a,LCD_FUNCTION_SET+LCD_DATA_LEN_8+LCD_DISP_LINES_2+LCD_FONT_8
 	call lcd_putcmd
 	ld a,LCD_DISPLAY_ON_OFF_CONTROL+LCD_DISPLAY_ON+LCD_CURSOR_ON+LCD_BLINK_ON
 	call lcd_putcmd
 	ld a,LCD_CLEAR_DISPLAY
 	call lcd_putcmd
-    ld hl,message               ; load the message address into HL
-print:
-	call lcd_puts
+    ld hl,start_message         ; load the message address into HL
+	call puts
+loop:
+    call getchar                ; get a character from USB - will wait for a character
+    cp '\e'                     ; escape?
+    jp z,end                    ; yes - end
+    call putchar                ; echo to console
+    call lcd_putchar            ; echo to LCD
+    jr loop                     ; repeat
+    
 end:
+    ld a,'\n'                   ; add a line break
+    call putchar                ; to the console
     jp RESET                    ; jump to the reset address - will jump back to the monitor
 
-message: 
-    db "Hello world!",0         ; message to be printed, terminated by a 0
+start_message: 
+    db "Echoing console input to console and LCD - hit 'Esc' to quit\n",0
 
 
 lcd_putcmd:                     ; transmit character in A to the control port
