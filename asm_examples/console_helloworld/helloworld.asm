a0951922543d8bb994568bf911f72cc4161a783c
Stephen Willcock <stephen.willcock@gmail.com>
2025-06-15 08:55:01 +0100

    console_helloworld

--- asm_examples/console_helloworld/helloworld.asm
+++ asm_examples/console_helloworld/helloworld.asm
@@ -1,18 +1,26 @@
-include "../lib/marvin.inc"     ; load all the definitions needed to interact with the monitor program
+; load all the definitions needed to interact with the monitor program
+include "../lib/marvin.inc"     
 include "../lib/beanzee.inc"
 
-org RAMSTART                    ; this is the default location for a BeanZee standalone assembly program 
+; this is the default location for a BeanZee standalone assembly program 
+org RAMSTART
 
 start:
-    ld hl,message               ; load the message address into HL
+    ; load the message address into HL
+    ld hl,message
 print:
-    call puts                   ; call the monitor puts routine, which will print the message pointed to by HL
+    ; call the monitor puts routine, which will print the message pointed to by HL
+    call puts
 wait:
-    call getchar                ; call the monitor getchar to get a character from the console input
-                                ; it will wait until a character is received
+    ; call the monitor getchar to get a character from the console input
+    ; it will wait until a character is received
+    call getchar
 end:
-    jp RESET                    ; jump to the reset address - will jump back to the monitor
+    ; jump to the reset address - will jump back to the monitor
+    jp PROMPT
 
 message: 
-    db "\nHello world!\n"     ; message to be printed 
-    db "(hit any key)\n\n",0    ; must be terminated by a 0
+    ; message to be printed 
+    ; must be terminated by a 0
+    db "\nHello world!\n"     
+    db "(hit any key)\n\n",0
