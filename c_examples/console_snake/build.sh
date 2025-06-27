3111f102b1f913890af08fb0a4c273341ab74388
Stephen Willcock <stephen.willcock@gmail.com>
2025-06-27 07:46:25 +0100

    C examples use .map files

--- c_examples/console_snake/build.sh
+++ c_examples/console_snake/build.sh
@@ -10,4 +10,4 @@ target="../lib/$t.asm"
 org=0x8000
 reset=0x0000
 
-zcc +z80 -clib=classic $f.c $target -pragma-define:CRT_ORG_CODE=$org -pragma-define:CRT_ON_EXIT=$reset -create-app -m -Cz--ihex -o=$f.o
+zcc +z80 -clib=classic $f.c $target -pragma-define:CRT_ORG_CODE=$org -pragma-define:CRT_ON_EXIT=$reset -create-app -m -Cz--ihex -o=$f-$t.o
