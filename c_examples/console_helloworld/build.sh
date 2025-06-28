afd685d8c68ab6ba8093b32ad43a6eaa46dc3729
Stephen Willcock <stephen.willcock@gmail.com>
2025-06-28 07:33:19 +0100

    update build process for c examples

--- c_examples/console_helloworld/build.sh
+++ c_examples/console_helloworld/build.sh
@@ -1,14 +1,21 @@
-# usage: ./build.sh target(.asm)
-# e.g.:  ./build.sh beanzee
-#        ./build.sh beanboard.asm
+# usage: 
+# with or without extension
+#  ./build.sh beanzee
+#  ./build.sh beanzee.asm
+# provide an org value
+#  ./build.sh beanzee $8000
+# defaults to 0x8000
 
-# set -x #echo on
+set -x #echo on
 
-f="helloworld" # source filename
-t=${1%.*} #extract base filename of target hardware
-target="../lib/$t.asm"
 org=0x8000
 reset=0x0000
+f=${1%.*} #extract base filename
+target="../lib/$f.asm"
 
-zcc +z80 -clib=classic $f.c $target -pragma-define:CRT_ORG_CODE=$org -pragma-define:CRT_ON_EXIT=$reset -create-app -m -Cz--ihex -o=$f-$t.o
-
+if [ $# -gt 1 ]
+then
+    zcc +z80 -clib=classic main.c $target -pragma-define:CRT_ORG_CODE=$2 -pragma-define:CRT_ON_EXIT=$reset -create-app -m -Cz--ihex -o=$f.o
+else
+    zcc +z80 -clib=classic main.c $target -pragma-define:CRT_ORG_CODE=$org -pragma-define:CRT_ON_EXIT=$reset -create-app -m -Cz--ihex -o=$f.o
+fi
