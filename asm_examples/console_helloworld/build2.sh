e5e12670da9fe1952f935438a88a3f7ac93d7283
Stephen Willcock <stephen.willcock@gmail.com>
2025-06-28 10:08:53 +0100

    Update build2.sh

--- asm_examples/console_helloworld/build2.sh
+++ asm_examples/console_helloworld/build2.sh
@@ -1,13 +1,27 @@
-# usage: ./build.sh target(.asm)
-# e.g.:  ./build.sh beanzee
-#        ./build.sh beanboard.asm
+# experimental build script using zcc rather than calling the tools directly
+
+# usage: 
+# with or without extension
+#  ./build.sh beanzee
+#  ./build.sh beanzee.asm
+# provide an org value
+#  ./build.sh beanzee $8000
+# defaults to 0x8000
 
 set -x #echo on
 
-f="helloworld" # source filename
-t=${1%.*} #extract base filename of target hardware
-target="../lib/$t.asm"
 org=0x8000
-reset=0x0000
+f=${1%.*} #extract base filename
 
-zcc +z80 $f.asm $target --no-crt -create-app -m -Cz--org=$org -Cz--ihex -o=$f-$t.o
+if [ $# -gt 1 ]
+then
+    # z88dk-z80asm -l -b -m -DORGDEF=$2 $f.asm -Ooutput
+    # z88dk-appmake +hex --org $2 -b output/$f.bin -o output/$f.ihx
+    zcc +z80 $f.asm --no-crt -create-app -m --list -Ca-DORGDEF=$2 -Cz--org=$2 -Cz--ihex -o=output/$f.bin
+    hexdump -C output/$f.bin > output/$f.hex
+else
+    # z88dk-z80asm -l -b -m -DORGDEF=$org $f.asm -Ooutput
+    # z88dk-appmake +hex --org $org -b output/$f.bin -o output/$f.ihx
+    zcc +z80 $f.asm --no-crt -create-app -m --list -Ca-DORGDEF=$org -Cz--org=$org -Cz--ihex -o=output/$f.bin
+    hexdump -C output/$f.bin > output/$f.hex
+fi
