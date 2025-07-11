972f33891e8d1a95241589ec2530211c210c1a64
Stephen Willcock <stephen.willcock@gmail.com>
2025-07-11 08:19:31 +0100

    build automation

---
+++ asm_examples/build.sh
@@ -0,0 +1,7 @@
+d=${PWD}
+cd $d/beanboard_echo && ./build.sh
+cd $d/beanboard_gpio && ./build.sh
+cd $d/beanboard_helloworld && ./build.sh
+cd $d/beanboard_keyscan && ./build.sh
+cd $d/beanboard_lcdcursor && ./build.sh
+cd $d/console_helloworld && ./build.sh
