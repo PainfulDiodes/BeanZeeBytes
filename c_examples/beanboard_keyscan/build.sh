6228cd58644a0ea7defdb49371fd1e8c2b345257
Stephen Willcock <stephen.willcock@gmail.com>
2025-07-11 07:49:38 +0100

    simplify c build scripts

--- c_examples/beanboard_keyscan/build.sh
+++ c_examples/beanboard_keyscan/build.sh
@@ -1,21 +1,24 @@
 # usage: 
-# with or without extension
-#  ./build.sh beanzee
-#  ./build.sh beanzee.asm
-# provide an org value
-#  ./build.sh beanzee $8000
+#  ./build.sh $8000
 # defaults to 0x8000
+#  ./build.sh
 
 # set -x #echo on
 restart=0x0010
-f=${1%.*} #extract base filename
 
-if [ $# -gt 1 ]
-then # target,org
-    org=$2
+if [ $# -gt 0 ]
+then
+    org=$1
 else # target
     org=0x8000
 fi
 
+f=beanboard
+
 zcc +z80 -clib=classic main.c ../lib/$f.asm -pragma-define:CRT_ORG_CODE=$org -pragma-define:CRT_ON_EXIT=$restart -create-app -m -Cz--ihex -o=output/$f.o
 z88dk-dis -o $org -x output/$f.map -x ../lib/$f.map output/$f.bin >output/$f.dis
+
+# f=beanzee
+
+# zcc +z80 -clib=classic main.c ../lib/$f.asm -pragma-define:CRT_ORG_CODE=$org -pragma-define:CRT_ON_EXIT=$restart -create-app -m -Cz--ihex -o=output/$f.o
+# z88dk-dis -o $org -x output/$f.map -x ../lib/$f.map output/$f.bin >output/$f.dis
