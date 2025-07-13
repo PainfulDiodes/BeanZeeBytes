9dde21bec20855906e964a60c3efcfde58d648e2
Stephen Willcock <stephen.willcock@gmail.com>
2025-07-13 10:14:40 +0100

    refactor build scripts

--- c_examples/beanboard_echo/build.sh
+++ c_examples/beanboard_echo/build.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/env bash
-
-# usage: ./build.sh org
-# e.g.:  ./build.sh 0x8000
-# org is optional
-
-if [[ $# -gt 0 ]]
-then
-    org=$1
-fi
-
-../lib/build.sh beanboard $org
+../lib/build.sh beanboard $@
--- c_examples/build.sh
+++ c_examples/build.sh
@@ -1,13 +1,6 @@
-if [[ $# -gt 0 ]]
-then
-    org=$1
-else
-    org=0x8000
-fi
-
 for dir in ${PWD}/*; do  
     if [ -d "$dir/output" ]; then
-        echo $dir
-        cd $dir && ./build.sh $org
+        echo $dir $@
+        cd $dir && ./build.sh $@
     fi
 done
