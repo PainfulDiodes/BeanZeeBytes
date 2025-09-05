75bcb540177b3344b541e719e046d06fe234357f
Stephen Willcock <stephen.willcock@gmail.com>
2025-09-05 08:13:27 +0100

    Refactor SPI implementation: simplify and fix errors

--- asm_examples/beanboard_spi/main.asm
+++ asm_examples/beanboard_spi/main.asm
@@ -1,181 +1,22 @@
+
 start:
     ; Initialize SPI interface
     call spi_init
-    
-    ld hl,start_message
-    call puts
-loop:
-    ; Print menu
-    ld hl,menu_message
-    call puts
-    
-    ; Get command
-    call getchar
-    call putchar
-    
-    ; Check command
-    cp 'w'            ; Write command
-    jr z,write_spi
-    cp 'r'            ; Read command
-    jr z,read_spi
-    cp '\e'           ; Escape
-    jr z,end
-    
-    ; Invalid command, show error
-    ld hl,error_message
-    call puts
-    jr loop
-
-write_spi:
-    ; Get hex byte to write
-    ld hl,write_prompt
-    call puts
-    
-    ; first hex char
-    call getchar
-    call putchar
-    cp '\e'
-    jr z,end
+    ; HL points to test string
+    ld hl,test_string
+send_loop:
+    ld a,(hl)
+    or a
+    jr z,done
     ld b,a
-    
-    ; second hex char
-    call getchar
-    call putchar
-    cp '\e'
-    jr z,end
-    
-    ; Convert ASCII hex to binary
-    call convert_ascii_hex_to_bin
-    
-    ; Write to SPI
     call spi_select
+    ld a,b
     call spi_write
     call spi_deselect
-    
-    ; Show confirmation
-    ld hl,write_done
-    call puts
-    jr loop
-
-read_spi:
-    ; Read from SPI
-    call spi_select
-    call spi_read
-    call spi_deselect
-    
-    ; Save read value
-    ld b,a
-    
-    ; Show read value
-    ld hl,read_result
-    call puts
-    
-    ; Convert to hex and display
-    ld a,b
-    call print_hex_byte
-    
-    ; New line
-    ld a,'\n'
-    call putchar
-    jr loop
-
-end:
-    ; add a line break at the console
-    ld a,'\n'
-    call putchar
-    ; jump to the reset address - will jump back to the monitor
+    inc hl
+    jr send_loop
+done:
     jp WARMSTART
 
-; Print a byte as two hex characters
-; Input: A = byte to print
-print_hex_byte:
-    push af
-    srl a
-    srl a
-    srl a
-    srl a
-    call print_hex_digit
-    pop af
-    and $0F
-    call print_hex_digit
-    ret
-
-; Print a single hex digit
-; Input: A = value 0-15
-print_hex_digit:
-    cp 10
-    jr nc,_print_hex_digit_letter
-    add '0'
-    jr _print_hex_digit_print
-_print_hex_digit_letter:
-    add 'A'-10
-_print_hex_digit_print:
-    jp putchar
-
-start_message: 
-    db "SPI Interface Test Program\n",0
-menu_message:
-    db "\nCommands: w=Write, r=Read, ESC=Exit\n",0
-error_message:
-    db "Invalid command\n",0
-write_prompt:
-    db "Enter byte to write (hex): ",0
-write_done:
-    db "\nByte written to SPI\n",0
-read_result:
-    db "Byte read from SPI: ",0
-
-; Converts two ASCII hex chars: B (high nibble), A (low nibble) to a single byte in A
-; Input:  B = ASCII hex (high nibble), A = ASCII hex (low nibble)
-; Output: A = combined binary byte
-
-convert_ascii_hex_to_bin:
-    push bc             ; Save BC
-    ld c, a             ; Save low nibble (A) in C
-
-    ; Convert high nibble in B
-    ld a, b
-    call convert_nibble  ; Result in A
-    rlca                ; Shift left 4 bits
-    rlca
-    rlca
-    rlca
-    ld b, a             ; Store high nibble in B
-
-    ; Convert low nibble in C
-    ld a, c
-    call convert_nibble  ; Result in A
-
-    ; Combine nibbles
-    or b                ; A = (high << 4) | low
-
-    pop bc              ; Restore BC
-    ret
-
-; --- Helper: Convert ASCII hex in A to binary nibble (0-15) ---
-convert_nibble:
-    cp '0'
-    jr c, invalid_nibble
-    cp '9'+1
-    jr c, is_digit
-    cp 'A'
-    jr c, invalid_nibble
-    cp 'F'+1
-    jr c, is_upper
-    cp 'a'
-    jr c, invalid_nibble
-    cp 'f'+1
-    jr nc, invalid_nibble
-    sub 'a'-10
-    ret
-is_digit:
-    sub '0'
-    ret
-is_upper:
-    sub 'A'-10
-    ret
-invalid_nibble:
-    xor a
-    ret
-
-
+test_string:
+    db 0b10101010,0xf0,0
--- asm_examples/beanboard_spi/spi.asm
+++ asm_examples/beanboard_spi/spi.asm
@@ -4,8 +4,6 @@
 SPI_SCK     equ 0
 ; Master Out Slave In (output)
 SPI_MOSI    equ 1
-; Master In Slave Out (input)
-SPI_MISO    equ 2
 ; Chip Select (output)
 SPI_CS      equ 3
 
@@ -14,7 +12,6 @@ SPI_CS      equ 3
 ; Destroys: AF
 spi_init:
     ; Set initial pin states (CS high, CLK low, MOSI low)
-    ; CS bit set, others clear
     ld a,1 << SPI_CS
     out (GPIO_OUT),a
     ret
@@ -35,76 +32,24 @@ spi_deselect:
     out (GPIO_OUT),a
     ret
 
-; Write and read a byte over SPI
-; Input: A = byte to send
-; Output: A = byte received
-; Destroys: BC
-spi_transfer:
-    ; Initialize bit counter
-    ld b,8
-    ; Save input byte
-    ld c,a
-_spi_transfer_bit_loop:
-    ; Put MSB on MOSI
-    ; Rotate left through carry
-    rlc c
-    ; Start with all pins low
-    ld a,0
-    jr nc,_spi_transfer_output_bit
-    ; Set MOSI if carry was set
-    or 1 << SPI_MOSI
-_spi_transfer_output_bit:
-    ; Output MOSI
-    out (GPIO_OUT),a
 
-    ; Generate clock pulse (rising edge)
-    ; Set clock high
+; Write a byte over SPI (no readback)
+; Input: A = byte to send
+; Destroys: AF, B
+spi_write:
+    ld b,8
+spi_write_bit:
+    rlca
+    ld d,a
+    ld a,0
+    jr nc,spi_write_mosi_low
+    or 1 << SPI_MOSI
+spi_write_mosi_low:
+    out (GPIO_OUT),a
     or 1 << SPI_SCK
     out (GPIO_OUT),a
-    
-    ; Read MISO
-    ; Read GPIO input
-    in a,(GPIO_IN)
-    ; Isolate MISO bit
-    and 1 << SPI_MISO
-    jr z,_spi_transfer_miso_low
-    ; Set carry if MISO was high
-    scf
-    jr _spi_transfer_store_bit
-_spi_transfer_miso_low:
-    ; Set carry
-    scf
-    ; Complement carry to clear it
-    ccf
-_spi_transfer_store_bit:
-    ; Store received bit
-    rr c
-
-    ; Generate clock pulse (falling edge)
-    ; Clear clock 
     and ~(1 << SPI_SCK)
     out (GPIO_OUT),a
-
-    ; Next bit
-    ; Decrement counter and loop if not zero
-    djnz _spi_transfer_bit_loop
-    
-    ; Return received byte in A
-    ld a,c
-    ret
-
-; Write a byte over SPI (no read)
-; Input: A = byte to send
-; Destroys: AF, BC
-spi_write:
-    call spi_transfer
-    ret
-
-; Read a byte over SPI (sending 0xFF)
-; Output: A = byte received
-; Destroys: AF, BC
-spi_read:
-    ; Send all 1's while reading
-    ld a,0xFF
-    call spi_transfer
+    ld a,d
+    djnz spi_write_bit
     ret
