88a2aa7bf89885bb3f929334132214cb5ed365d0
Stephen Willcock <stephen.willcock@gmail.com>
2025-10-11 13:06:07 +0100

    RA8875 clean up comments

--- asm_examples/beanboard_RA8875/main.asm
+++ asm_examples/beanboard_RA8875/main.asm
@@ -5,30 +5,20 @@ start:
     call getchar
     call ra8875_set_init_state
 
-    ; set CS low
     call ra8875_set_selected_state
-    ; send RA8875_CMDWRITE (0x80)
     ld a,RA8875_CMDWRITE
     call ra8875_write
     ; send 0x00 (register number)
     ld a,0x00
     call ra8875_write
-    ; end SPI
-    ; set CS high
     call ra8875_set_deselected_state
 
-    ; set CS low
     call ra8875_set_selected_state
-    ; send RA8875_DATAREAD (0x40)
     ld a,RA8875_DATAREAD
     call ra8875_write
-    ; receive value (sending 0x00)
     call ra8875_read
-    ; preserve returned value
     push af
-    ; set CS high
     call ra8875_set_deselected_state
-
     pop af
 
     cp 0x75
