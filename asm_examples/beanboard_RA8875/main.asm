c36d8e60cefb6070d1b314fb88d736e9c6c51650
Stephen Willcock <stephen.willcock@gmail.com>
2025-10-11 14:58:08 +0100

    ra8875 tidy main.asm

--- asm_examples/beanboard_RA8875/main.asm
+++ asm_examples/beanboard_RA8875/main.asm
@@ -2,7 +2,7 @@ start:
     ; reset needs to be held for a short time, hence getchar before moving on
     ; in reality we may want a delay
     call ra8875_setstate_reset
-    ld hl,message
+    ld hl,start_message
     call puts
     call getchar
     call ra8875_setstate_inactive
@@ -25,26 +25,25 @@ start:
 
     call putchar_hex
 
-    cp 0x75
+    cp 0x75 ; TODO: replace magic number
     jp nz,error_0
 
-    ; do some more stuff
-
-    ld hl,success_message
+    ld hl,init_success_message
     call puts
+
     jp done
 
 error_0:
-    ld hl,error_message_0
+    ld hl,init_error_message
     call puts
 
 done:
     jp WARMSTART
 
-message:
+start_message:
     db "RA8875 test\nHit any key to continue...\n",0
-error_message_0:
-    db "\nRA8875 init fail\n",0
+init_error_message:
+    db "\nRA8875 init fail - expected 0x75\n",0
 
-success_message:
-    db "\nRA8875 success\n",0
+init_success_message:
+    db "\nRA8875 init success\n",0
