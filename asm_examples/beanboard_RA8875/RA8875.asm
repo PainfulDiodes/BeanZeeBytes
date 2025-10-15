89b92956adac5e2c3bb4d20c1a0022cd04cbfea4
Stephen Willcock <stephen.willcock@gmail.com>
2025-10-15 08:32:36 +0100

    RA8875_HNDR, RA8875_HSTR

--- asm_examples/beanboard_RA8875/RA8875.asm
+++ asm_examples/beanboard_RA8875/RA8875.asm
@@ -41,6 +41,28 @@ RA8875_HNDFTR_DE_LOW equ 0x80
 RA8875_HNDFTR_800x480 equ RA8875_HNDFTR_DE_HIGH + 0 ; polarity + fine tuning value
 RA8875_HNDFTR_VAL equ RA8875_HNDFTR_800x480
 
+; REG[16h] LCD Horizontal Non-Display Period Register (HNDR)
+RA8875_HNDR equ 0x16
+; Horizontal Non-Display Period(HNDP) Bit[4:0] (pixels) = (HNDR + 1)x8+(HNDFTR/2+1)x2 + 2
+; (hsync_nondisp - hsync_finetune - 2) /8)
+RA8875_HNDR_800x480 equ (26-0-2)/8 ; =3
+RA8875_HNDR_VAL equ RA8875_HNDR_800x480
+
+; REG[17h] HSYNC Start Position Register (HSTR)
+RA8875_HSTR equ 0x17
+; HSYNC Start Position[4:0]
+; The starting position from the end of display area to the beginning of HSYNC. 
+; Each level of this modulation is 8-pixel. HSYNC Start Position(pixels) = (HSTR + 1)x8
+; hsync_start / 8 - 1
+RA8875_HSTR_800x480 equ 32 / 8 - 1 ; =3
+RA8875_HSTR_VAL equ RA8875_HSTR_800x480 
+
+
+RA8875_HPWR equ 0x18
+RA8875_HPWR_LOW equ 0x00
+RA8875_HPWR_HIGH equ 0x80
+
+
 ; REG[88h] PLL Control Register 1 (PLLC1)
 RA8875_PLLC1 equ 0x88 
 RA8875_PLLC1_PLLDIV1 equ 0x00
@@ -330,6 +352,12 @@ ra8875_horizontal_settings_init:
     call ra8875_write_reg
     ld a,RA8875_HNDFTR
     ld b,RA8875_HNDFTR_VAL
+    call ra8875_write_reg
+    ld a,RA8875_HNDR
+    ld b,RA8875_HNDR_VAL
+    call ra8875_write_reg
+    ld a,RA8875_HSTR
+    ld b,RA8875_HSTR_VAL
     call ra8875_write_reg
     pop bc
     pop af
