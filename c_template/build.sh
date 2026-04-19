becca0d55f7f8316dfe64b46ae0383b4e9754a9e
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-04-19 09:22:03 +0100

    clarify C build scripts

--- c_template/build.sh
+++ c_template/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 org=${1:-0x8000}
+marvin_warmstart=0x0040
 
 zcc +z80 -clib=classic main.c marvin.asm \
     -pragma-define:CRT_ORG_CODE=$org \
-    -pragma-define:CRT_ON_EXIT=0x0040 \
+    -pragma-define:CRT_ON_EXIT=$marvin_warmstart \
     -create-app -m -Cz--ihex -o=output/main.bin
 z88dk-dis -o $org -x output/main.map -x marvin.inc \
     output/main.bin > output/main.dis
