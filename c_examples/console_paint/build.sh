0589c09b14dffc26463d1c668ad99a421126db43
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-04-19 07:55:03 +0100

    build simplification / Marvin 1.3 - clean up C examples

--- c_examples/console_paint/build.sh
+++ c_examples/console_paint/build.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 org=${1:-0x8000}
 
-zcc +z80 -clib=classic main.c ../lib/beanboard.asm \
+zcc +z80 -clib=classic main.c ../lib/marvin.asm \
     -pragma-define:CRT_ORG_CODE=$org \
     -pragma-define:CRT_ON_EXIT=0x0040 \
-    -create-app -m -Cz--ihex -o=output/beanboard.bin
-z88dk-dis -o $org -x output/beanboard.map -x ../../lib/marvin.inc \
-    output/beanboard.bin > output/beanboard.dis
-
-zcc +z80 -clib=classic main.c ../lib/beanzee.asm \
-    -pragma-define:CRT_ORG_CODE=$org \
-    -pragma-define:CRT_ON_EXIT=0x0040 \
-    -create-app -m -Cz--ihex -o=output/beanzee.bin
-z88dk-dis -o $org -x output/beanzee.map -x ../../lib/marvin.inc \
-    output/beanzee.bin > output/beanzee.dis
+    -create-app -m -Cz--ihex -o=output/main.bin
+z88dk-dis -o $org -x output/main.map -x ../../lib/main.inc \
+    output/main.bin > output/main.dis
--- c_examples/lib/beanzee.asm
+++
@@ -1,1 +0,0 @@
-include "marvin.asm"
